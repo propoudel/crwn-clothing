755c35a4d25049bfae7c47d7e9c5b715073374c2
Tuklal Poudel <tuklal@pegotec.net>
2020-07-08 21:07:27 +0545

    added files for home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
+import './homepage.styles.scss';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
