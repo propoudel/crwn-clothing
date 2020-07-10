02fa0fc927e982d40b9dbba5e3cef8deea61add0
Tuklal Poudel <tuklal@pegotec.net>
2020-07-10 14:01:26 +0545

    Implemented firebase utils, included ability to store authenticated user  into firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -21,9 +21,25 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount(){
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+
+
+            if (userAuth){
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot( (snapshot) => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    })
+                });
+
+            }
+
+            this.setState({ currentUser: userAuth })
+
         });
     }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import './sing-in-and-sign-up.styles.scss';
+import './sign-in-and-sign-up.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+
+import SignUp from '../../components/sign-up/sign-up.component';
+
 
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
-        <SignIn/>
+        <SignIn />
+        <SignUp />
 
     </div>
 );
